6a6b2b2aa51acd6419d8cf35aa19d3c5100a99d9
ludovic nay <ludovic.nay@ynov.com>
2018-02-13 15:00:25 +0100

    Ajout du module Twitter

---
+++ serveur.js
@@ -0,0 +1,8 @@
+var Twitter = require('twitter');
+
+var twitter = new Twitter({
+    consumer_key: '	y7UVM1GSCsEePysM1d9b81Kjd',
+    consumer_secret: '	1yncOY2g3PoF2bXowZmk6UcIaoRTYp5XVWRUqMPmDhBYgklVTo',
+    access_token_key: '	379700834-ehvg47TIcNZnKkrjJX0Ik6E0fFj4duIgSRbnpXRi',
+    access_token_secret: 'pYfPfJLicHcVnivOoBunR8E3Ujvr6NhmaJJ9DuWpyJMcx'
+});
